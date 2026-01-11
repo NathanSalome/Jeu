1294f7553a3125d70195a599c8293ff8a6cf9e6d
Nathan <nathansalome.pro@outlook.com>
2026-01-12 00:37:24 +0100

    Patch Compteur nbMots

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,39 @@
 let span = document.querySelector(".mots");
 let input = document.querySelector(".text");
 let scoreSpan = document.querySelector(".score");
-let btn = document.getElementById("btn")
+let btn = document.getElementById("btn");
+let spanNbMots = document.querySelector(".nbmots");
 
 let score = 0;
+let nbmot = 0;
 
 span.innerHTML = `${MotAleatoire()}` // afficher le mots sur la page 
 scoreSpan.innerHTML = `${score}` // afficher le score sur la page 
+spanNbMots.innerHTML = `${nbmot}`
+
 
 function MotAleatoire(){
     let motsAl = Math.floor(Math.random() * mots.length);
     return mots[motsAl];
 }
 
+function MajDonne(){
+    span.innerHTML = `${MotAleatoire()}`
+    spanNbMots.innerHTML = `${nbmot}`
+    input.value = ""
+}
 
 btn.addEventListener("click", (event) => {
     let MotActu = span.textContent
     console.log(MotActu)
     if(input.value === MotActu){
         score++
-        console.log("Vrai")
+        nbmot++
         scoreSpan.innerHTML = `${score}`
-        span.innerHTML = `${MotAleatoire()}`
+        MajDonne()
     }
     else{
-        score++
-        console.log("faux")
-        span.innerHTML = `${MotAleatoire()}`
+        nbmot++
+        MajDonne()
     }
 });
